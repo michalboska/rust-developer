bb9dc572ed65785fa60c34b5678adccb67444a10
Michal Boska <michal.boska@external.erstegroup.com>
2023-10-05 09:40:59 +0200

    Add random variable assignment to test the debugger

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,6 @@
+use rand::random;
+
 fn main() {
-    println!("Hello, rusty world!");
+    let random_val = random::<u8>(); // temporary assignment to a variable to test the debugger.
+    println!("Hello, rusty world! Random value is {}", random_val);
 }
