14692e3744fa463e2e5342d5e205edf7f2c59ff8
Michal Boska <michal.boska@external.erstegroup.com>
2023-10-09 11:35:59 +0200

    ex02 final

--- ex02_slugify/src/main.rs
+++ ex02_slugify/src/main.rs
@@ -1,3 +1,31 @@
+use std::{env, io};
+use std::process::exit;
+
+use slug::slugify;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Missing command line argument!");
+        exit(1);
+    }
+    let transform_fn = get_transform_fn(String::from(&args[1]));
+    let mut buffer: String = String::from("");
+    let _ = io::stdin().read_line(&mut buffer);
+    println!("Result: {}", transform_fn(buffer));
+}
+
+fn get_transform_fn(operation: String) -> fn(String) -> String {
+    return match operation.as_str() {
+        "lowercase" => |s| { s.to_lowercase() },
+        "uppercase" => |s| { s.to_uppercase() },
+        "no-spaces" => |s| { s.replace(" ", "") },
+        "slugify" => |s| { slugify(s) },
+        "length" => |s| { s.chars().count().to_string() },
+        "reverse" => |s| { s.chars().rev().collect() },
+        _ => {
+            println!("Command line argument must be one of: lowercase,uppercase,no-spaces,slugify,length,reverse");
+            exit(1);
+        }
+    };
 }
